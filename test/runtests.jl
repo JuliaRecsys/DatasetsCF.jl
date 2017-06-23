1dfedb8ddcdb73cbb660a46d9240d7cf1b550f35
Filipe Braida <filipebraida@gmail.com>
2017-06-23 12:53:16 -0300

    adding basics tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,4 +2,16 @@ using DatasetsCF
 using Base.Test
 
 # write your own tests here
-@test 1 == 2
+ds = DatasetsCF.MovieLens()
+@test ds.users == 943
+@test ds.items == 1682
+@test length(ds.preferences.possibles) == 5
+@test ds.preferences.min == 1
+@test ds.preferences.max == 5
+
+ds = DatasetsCF.MovieLens1M()
+@test ds.users == 6040
+@test ds.items == 3706
+@test length(ds.preferences.possibles) == 5
+@test ds.preferences.min == 1
+@test ds.preferences.max == 5
